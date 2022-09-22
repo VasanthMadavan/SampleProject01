f331465e396a09ca64b141c140ff96e917fc3639
VasanthMadavan <vasanthvibu52@gmail.com>
2022-09-22 18:34:10 +0530

    newcode

---
+++ src/org/lop/Sample.java
@@ -0,0 +1,39 @@
+package org.lop;
+
+public class Sample {
+	
+	public void empId() {
+	System.out.println("hsggas");
+	}
+	
+	public void empName() {
+	System.out.println("mkidhwd");
+	}
+	
+	public void empDob() {
+    System.out.println("jdhfh");
+	}
+	
+	public void empPhone() {
+	System.out.println("jsbus");
+	}
+	
+	public void empEmail() {
+	System.out.println("ihsdhw");
+	}
+	
+	public void empAddress() {
+	System.out.println("idhsud");
+	}
+	
+	public static void main(String[] args) {
+	Sample  s = new Sample();
+	s.empId();
+	s.empName();
+	s.empDob();
+	s.empPhone();
+	s.empEmail();
+	s.empAddress();
+	}
+
+}
